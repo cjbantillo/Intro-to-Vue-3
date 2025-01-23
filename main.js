ce6f38d622de4a60840dfb9fa747949b7f17a274
cjbantillo <bantillocj1@gmail.com>
2025-01-23 11:01:24 +0800

    refactor: improve inventory and sale status comments for clarity

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inventory: 100,
-            // solution
             onSale: true
-            // solution
         }
     }
 })

598537637c9346f4deadceaf9766bddde291af84
cjbantillo <bantillocj1@gmail.com>
2025-01-23 19:09:39 +0800

    feat: add product variants and sizes to inventory display

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                 {id: 1, color: 'green'},
+                  {id: 2, color: 'blue'},
+                 ],
+            sizes: ['Small', 'Medium', 'Large'],
          
         }
     }

264d77d5662ae7c362b56b04f490e3a7de825981
cjbantillo <bantillocj1@gmail.com>
2025-01-23 20:50:36 +0800

    feat: add minus cart functionality and update button label

--- main.js
+++ main.js
@@ -18,6 +18,9 @@ const app = Vue.createApp({
         },
         updateImage(variantImage){
             this.image = variantImage
+        },
+        minusCart(){
+            this.cart -= 1
         }
     }
 })

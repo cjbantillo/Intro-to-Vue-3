59eb74ae9e65d03616c3e52afe5f8df833af39ae
cjbantillo <bantillocj1@gmail.com>
2025-01-23 09:05:41 +0800

    Refactor Vue app structure and update product display

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+           product: 'Socks',
+           details: ['50% cotton', '30% wool', '20% polyester'],
+           description: 'A pair of warm, fuzzy socks',
+        }
+    }
+})

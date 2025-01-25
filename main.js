781be5a30b1d6ed1aa498ae393918d34c26a7907
cjbantillo <bantillocj1@gmail.com>
2025-01-25 13:24:17 +0800

    feat: enhance product display with dynamic variant selection and stock status

--- main.js
+++ main.js
@@ -4,12 +4,23 @@ const app = Vue.createApp({
       cart: 0,
       product: "Socks",
       brand: "Vue Mastery",
-      image: "./assets/images/socks_blue.jpg",
+      selectedVariant: 0,
       inStock: false,
+      onsale: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+        {
+          id: 2234,
+          color: "Green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "Blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
       ],
     };
   },
@@ -24,8 +35,25 @@ const app = Vue.createApp({
       }
     },
     // solution
-    updateImage(variantImage) {
-      this.image = variantImage;
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+        return this.variants[this.selectedVariant].quantity;
+    },
+    sale() {
+        if (this.variants[this.selectedVariant].quantity > 0) {
+            return this.variants[this.selectedVariant].color + " " + this.product + " are on sale!";
+        }
+        return this.variants[this.selectedVariant].color + " " + this.product + " are not on sale!";
     },
   },
 });
